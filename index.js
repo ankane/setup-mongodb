bbf6972ddc90740aa84fb0a32e9504aec03bf30e
Andrew Kane <andrew@chartkick.com>
2020-11-10 23:11:01 -0800

    Sleep

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ if (process.platform == 'darwin') {
     run(`sudo apt-get install mongodb-org`);
   }
   run(`sudo systemctl start mongod`);
-  run(`for i in {1..20}; do mongo --eval "db.version()" && break; done`);
+  run(`for i in {1..20}; do mongo --eval "db.version()" && break; sleep 1; done`);
 }
