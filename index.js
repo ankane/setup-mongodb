3ddd217cee69c6001ea34c1193c176dc4fa20319
Andrew Kane <andrew@chartkick.com>
2020-11-10 22:02:11 -0800

    Start server on Mac

--- index.js
+++ index.js
@@ -5,19 +5,21 @@ function run(command) {
   execSync(command, {stdio: 'inherit'});
 }
 
-const mongoVersion = parseFloat(process.env['INPUT_MONGODB-VERSION'] || 13);
+const mongoVersion = parseFloat(process.env['INPUT_MONGODB-VERSION'] || 4.4);
 
 if (![4.4, 4.2, 4.0, 3.6, 3.4, 3.2].includes(mongoVersion)) {
   throw 'Invalid MongoDB version: ' + mongoVersion;
 }
 
 if (process.platform == 'darwin') {
-  run(`brew tap mongodb/brew`);
   run(`brew install mongodb-community@${mongoVersion}`);
+  run(`mongod --config /usr/local/etc/mongod.conf --fork`);
 } else {
-  run(`wget -qO - https://www.mongodb.org/static/pgp/server-${mongoVersion}.asc | sudo apt-key add -`);
-  run(`echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/${mongoVersion} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${mongoVersion}.list`);
-  run(`sudo apt-get update`);
-  run(`sudo apt-get install mongodb-org`);
+  if (mongoVersion != 4.4) {
+    run(`wget -qO - https://www.mongodb.org/static/pgp/server-${mongoVersion}.asc | sudo apt-key add -`);
+    run(`echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/${mongoVersion} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${mongoVersion}.list`);
+    run(`sudo apt-get update`);
+    run(`sudo apt-get install mongodb-org`);
+  }
   run(`sudo systemctl start mongod`);
 }
