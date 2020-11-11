ee43ad9c784371605d7ce165754ca59abadbd65f
Andrew Kane <andrew@chartkick.com>
2020-11-11 01:26:58 -0800

    Cleaner code

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function run(command) {
   execSync(command, {stdio: 'inherit'});
 }
 
-const mongoVersion = parseFloat(process.env['INPUT_MONGODB-VERSION'] || "4.4").toFixed(1);
+const mongoVersion = parseFloat(process.env['INPUT_MONGODB-VERSION'] || '4.4').toFixed(1);
 
 // TODO make OS-specific
-if (!["4.4", "4.2", "4.0", "3.6", "3.4", "3.2"].includes(mongoVersion)) {
+if (!['4.4', '4.2', '4.0', '3.6', '3.4', '3.2'].includes(mongoVersion)) {
   throw `MongoDB version not supported: ${mongoVersion}`;
 }
 
 if (process.platform == 'darwin') {
-  if (mongoVersion != "4.4") {
+  if (mongoVersion != '4.4') {
     // remove previous version
     run(`brew unlink mongodb-community`);
 
@@ -28,7 +28,7 @@ if (process.platform == 'darwin') {
   // set path
   run(`echo "${bin}" >> $GITHUB_PATH`);
 } else {
-  if (mongoVersion != "4.4") {
+  if (mongoVersion != '4.4') {
     // remove previous version
     run(`sudo rm /etc/apt/sources.list.d/mongodb-org-*.list`);
     run(`sudo apt-get purge mongodb-org*`);
