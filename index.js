f86a2f491e71309bf4514a033e39d609daaca3b5
Andrew Kane <andrew@chartkick.com>
2020-11-17 19:52:56 -0800

    Only update single list

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ if (process.platform == 'darwin') {
     // install new version
     run(`wget -qO - https://www.mongodb.org/static/pgp/server-${mongoVersion}.asc | sudo apt-key add -`);
     run(`echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME)/mongodb-org/${mongoVersion} multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-${mongoVersion}.list`);
-    run(`sudo apt-get update`);
+    run(`sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/mongodb-org-${mongoVersion}.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"`);
     run(`sudo apt-get install mongodb-org`);
   }
 
